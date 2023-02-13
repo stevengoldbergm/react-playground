a5613503a1bf42112d756649ff5eb627613852ad
SG_Demi <stevengoldbergm@gmail.com>
2023-02-12 22:49:38 -0600

    Added Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+// Create a modal here
+
+function Modal(props) {
+
+  return (
+    <>
+      <div className="modal">
+        <p> Are you sure? </p>
+        <button className="btn btn--alt" onClick={props.onCancel}>Cancel</button>
+        <button className="btn" onClick={props.onConfirm}>Confirm</button>
+      </div>
+    </>
+  );
+}
+
+export default Modal;
