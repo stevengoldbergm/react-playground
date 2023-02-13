d5ec08c106bdd57a689ca04d0306e1c1604d9763
SG_Demi <stevengoldbergm@gmail.com>
2023-02-13 00:02:24 -0600

    Moved TODO project into a folder for reference.

---
+++ TODO Project/src/App.js
@@ -0,0 +1,17 @@
+import Todo from './components/Todo'
+
+function App() {
+
+  return (
+    <>
+      <h1>My TODOS</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React Course' />
+      <Todo text='Read Nona the 9th' />
+      
+    </>
+  );
+}
+
+export default App;
