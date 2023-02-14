c3fd04b8848468763d07eefcbd270a20a1a4336f
SG_Demi <stevengoldbergm@gmail.com>
2023-02-14 14:30:28 -0600

    Set up app routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetUps";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
 function App() {
+  // localhost:3000/
+  // my-page.com
 
   return (
-    <>
-            
-    </>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} exact />
+
+          <Route path="/new-meetup" element={<NewMeetupsPage />} />
+
+          <Route path="/favorites" element={<FavoritesPage />} />
+
+          <Route path="/*" element={<AllMeetupsPage />} />
+        </Routes>
+      </Layout>
   );
 }
 
